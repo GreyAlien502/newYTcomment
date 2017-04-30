8ee7dee6dc81d664f2bea4b68ce492a6f80caa31
GreyAlien502 <GreyAlien502@gmail.com>
2017-04-30 02:32:30 -0400

    updating method to be simpler and more efficient

--- src/newYTcomment.js
+++ src/newYTcomment.js
@@ -1,8 +1,13 @@
 function tryToNovate(){
-	if(document.getElementsByClassName("yt-ui-menu-item yt-uix-menu-close-on-select comment-section-sort-menu-item yt-uix-sessionlink").length == 2){
-		document.getElementsByClassName("yt-uix-button yt-uix-button-size-default yt-uix-button-default yt-uix-menu-trigger")[0].click();
-		document.getElementsByClassName("yt-ui-menu-item yt-uix-menu-close-on-select comment-section-sort-menu-item yt-uix-sessionlink")[1].click();
-	}
+	try{
+		var newButton = document.querySelector('[data-menu_name="newest-first"]');
+		var selectButton = document.getElementsByClassName("comment-section-sort-menu")[0].children[0];
+		if(newButton.children[0].innerHTML != selectButton.children[0].innerHTML){
+			newButton.click();
+			selectButton.click();
+			newButton.click();
+		}
+	}catch(error){}
 	setTimeout(tryToNovate,1000);
 }
 tryToNovate();
